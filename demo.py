176b65d7e84b5f45e3f55f8904f7f96f4368356e
MaxChangInnodisk <max_chang@innodisk.com>
2022-08-05 11:50:48 +0800

    feat: refactory and change the outlook

--- demo.py
+++ demo.py
@@ -1,297 +1,78 @@
-import cv2, sys, os, time, logging, colorlog, random
-import numpy as np
-from openvino.inference_engine import IENetwork, IECore
+# -*- coding: utf-8 -*-
+import cv2, sys, os, time, logging
 from pipeline import Source
+from utils import *
 
-def config_logging():
-    logger = logging.getLogger()            # get logger
-    logger.setLevel(logging.DEBUG)       # set level
-    
-    if not logger.hasHandlers():    # if the logger is not setup
-        basic_formatter = logging.Formatter( "%(asctime)s [%(levelname)s] %(message)s (%(filename)s:%(lineno)s)", "%y-%m-%d %H:%M:%S")
-        formatter = colorlog.ColoredFormatter( "%(asctime)s %(log_color)s [%(levelname)-.4s] %(reset)s %(message)s %(purple)s (%(filename)s:%(lineno)s)", "%y-%m-%d %H:%M:%S")
-        # add stream handler
-        stream_handler = logging.StreamHandler()
-        stream_handler.setFormatter(formatter)
-        stream_handler.setLevel(logging.DEBUG)
-        logger.addHandler(stream_handler)
-
-def preprocess(image, size):
-    """ 
-    1. Resize to network image size
-    2. Change data layout from HWC to CHW
-    """
-    return cv2.resize(image, size).transpose((2, 0, 1))    
-
-def load_network(ie, model_path, weight_path, device="CPU"):
-    """
-    Load IR Model and Parse Information.
-    """
-    logging.info("---")
-    logging.info("Load Network")
-    t_start = time.time()
-    [ logging.info("\t- {}: {}".format(key, value)) for key, value in { "Model Path":model_path, "Weight Path":weight_path}.items() ]
-    net = ie.read_network(
-        model = model_path,
-        weights = weight_path
-    )
-    
-    input_blob = [ i_layer for i_layer in iter(net.input_info) ]
-    output_blob = [ o_layer for o_layer in iter(net.outputs) ]
-    batch, channel, height, width = net.input_info[input_blob[0]].input_data.shape
-    
-    logging.info("\t- Input Layer ({})".format( len(input_blob) ))
-    [ logging.info("\t\t- Name: {}".format( layer )) for layer in input_blob ]
-
-    logging.info("\t- Output Layer ({})".format( len(output_blob) ))
-    [ logging.info("\t\t- Name: {}".format( layer )) for layer in output_blob ]
-    
-    logging.info("\t- Get Input Information")
-    logging.info("\t\t- Input Shape: {}".format((batch, channel, height, width)))
-
-    exec_net = ie.load_network(
-        network = net, 
-        device_name = device
-    )
-    t_end = time.time()
-    logging.info("Loading Network Cost {:.3}s".format(t_end-t_start))
-
-    return net, input_blob, output_blob ,(batch, channel, height, width), exec_net
-
-def bbox_scaler(bbox, trg_height, trg_width):
-
-    x_l = int(trg_width * bbox[0])
-    y_l = int(trg_height * bbox[1])
-    x_u = int(trg_width * bbox[2])
-    y_u = int(trg_height * bbox[3])
-
-    x_l = x_l if x_l > 0 else 0
-    y_l = y_l if y_l > 0 else 0
-    x_u = x_u if x_u < trg_width else trg_width
-    y_u = y_u if y_u < trg_height else trg_height
-    
-    return x_l, y_l, x_u, y_u
-
-def get_random_color( format='bgr'):
-    r = random.randint(0,255)
-    g = random.randint(0,255)
-    b = random.randint(0,255)
-    return [r, g, b] if format=='rgb' else [b, g, r]
-
-def capture_temperature(exec_net):
-    try:
-        return round(float(exec_net.get_metric("DEVICE_THERMAL")), 3)
-    except Exception as e:
-        return "Not support Temperature"
-
-def capture_device_name(ie, device):
-    try: 
-        return ie.get_metric(metric_name="FULL_DEVICE_NAME", device_name=device)
-    except Exception as e: 
-        return ""
-
-def draw_device_info(org_image, ie, net, start_pos=None, title="Device Information"):
-    h,w,c = org_image.shape
-    padding = 10
-    # Combine data
-    info = [ title ]
-    info.append(" - {}: {}".format("Name", capture_device_name(ie, net[DEVICE])))
-    info.append(" - {}: {}".format("Temp", capture_temperature(net[EXEC])))
-    
-    # Draw
-    retval = cv2.getTextSize(title, FONT_FACE, FONT_SCALE, FONT_THICK)[0][1] + padding
-
-    if start_pos==None:
-        start_x, start_y = ( padding, (h-padding)-(retval*len(info)) )
-    else:
-        start_x, start_y = ( start_pos[0], (start_pos[1]-padding)-(retval*len(info)) )
-
-    # Get First Position
-    text_x, text_y = start_x, start_y
-    
-    for i in range(len(info)):
-        text_x, text_y = text_x, text_y + (retval)
-        cv2.putText(org_image, info[i], (text_x, text_y), FONT_FACE, FONT_SCALE, (0, 0, 0), FONT_THICK+1, cv2.LINE_AA)
-        cv2.putText(org_image, info[i], (text_x, text_y), FONT_FACE, FONT_SCALE, (0, 0, 255), FONT_THICK, cv2.LINE_AA)
-        
-    return org_image, (start_x, start_y)
-
-class DiscountInfo():
-    def __init__(self) -> None:
-
-        self.palette = dict()
-        self.informations = list()
-        self.mapping_table = dict()
-
-        self.define_mapping_table()
-        self.generate_palette()
-        
-    def define_mapping_table(self):
-        
-        # Define information
-        self.mapping_table = {
-            "male":{
-                "50+"   : "20% Off for Rolex Watches",
-                "30-50" : "30% Off for Trendy males",
-                "30-"   : "20% Off for Video Game",
-            },
-            "female":{
-                "50+"   : "25% Off for Jewelry",
-                "30-50" : "30% Off for Boutique package",
-                "30-"   : "20% Off for Wonmen's Clothes",
-            }
-        }
-
-    def generate_palette(self):
-        # Generate palette
-        for gender, whole_info in self.mapping_table.items():
-            for range, info in whole_info.items():
-                self.informations.append(info)
-                random_color = get_random_color()
-                
-                print(info, random_color)
-
-                self.palette.update( { info: random_color })
-
-    def get_palette(self):
-        return self.palette
-
-    def get_info_color(self, info):
-        if not (info in self.informations):
-            raise Exception("{} not in mapping table".format(info))
-        return self.palette[info]
-
-    def formulation(self, age):
-        if age <= 30: 
-            return "30-"
-        elif age >= 50:
-            return "50+"
-        else:
-            return "30-50"
-    
-    def __call__(self, age, gender):
-        """
-        Argument
-            - age
-                - type: int
-            - gender
-                - type: string
-        Return
-            - product
-                - type: String
-                - example: "20% Off for Rolex Watches"
-        """
-        return self.mapping_table[gender.lower()][self.formulation(age)]
+# Define Source
+SRC             = '/dev/video0'
+SRC_TYPE        = 'Video'
+LOGO            = 'assest/innodisk-logo.png'
 
 if __name__ == '__main__':
 
-    # Define OpenCV
-    CV_WIN          = "Face Age Gender"
-    FONT_FACE       = cv2.FONT_HERSHEY_SIMPLEX
-    FONT_SCALE      = 0.4
-    FONT_THICK      = 1
-    FULL_SCREEN     = False
-
-    # Define Key
-    PRI            = "PRI"
-    SEC            = "SEC"
-    ORG             = "ORG"
-
-    # PATH            = "PATH"
-    MODEL_PATH      = "MODEL_PATH"
-    WEIGHT_PATH     = "WEIGHT_PATH"
-    NET             = "NET"
-    EXEC            = "EXEC"
-    IN_BLOB         = "INPUT_BLOB"
-    OUT_BLOB        = "OUT_BLOB"
-    RESULT          = "RESULT"
-    DEVICE          = "DEVICE"
-    BATCH           = "BATCH"
-    CHANNEL         = "CHANNEL"
-    HEIGHT          = "HEIGHT"
-    WIDTH           = "WIDTH"
-
     # Main
-    import argparse, json
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", help="path to config", default="config.json")
-    parser.add_argument("-a", "--app", help="enable app", action="store_true")
-    args = parser.parse_args()
+    args = set_argparse()
 
     # Defince logger and cv windows
     config_logging()
     
     # Load Configuration
-    conf = None
-    with open(args.config, 'r') as f:
-        conf = json.load(f)
+    conf = read_json(args.config)
 
     # Update classes variable
     classes = conf["secondary"]["category"]
     thres = conf["primary"]["thres"]
 
-    # Define nets
-    nets = {
-        PRI: dict(),
-        SEC: dict(),
-        ORG: dict()
-    }
+    # Load All Network
+    nets = load_multi_network(conf)
 
-    # Load CPU extenstion
-    ie = IECore()
-
-    # Read IR model
-    for TRG, path, device in zip([ PRI, SEC ], [ conf["primary"]["path"], conf["secondary"]["path"] ], [ conf["primary"]["device"], conf["secondary"]["device"] ]):
-
-        nets[TRG][MODEL_PATH] = "{}.xml".format(path)
-        nets[TRG][WEIGHT_PATH] = "{}.bin".format(path)
-        nets[TRG][DEVICE] = device
-
-        (   nets[TRG][NET], 
-            nets[TRG][IN_BLOB], 
-            nets[TRG][OUT_BLOB], 
-            ( _, _, nets[TRG][HEIGHT], nets[TRG][WIDTH] ), 
-            nets[TRG][EXEC] ) = load_network(
-                                    ie = ie,
-                                    model_path = nets[TRG][MODEL_PATH],
-                                    weight_path = nets[TRG][WEIGHT_PATH],
-                                    device = nets[TRG][DEVICE]
-                                )
-    logging.info("Load All Network Sccessfully")
-    
-    # Instance Object
-    src = Source('/dev/video0', "Video")
-    app = DiscountInfo() if args.app else None
+    # Instance Source Object and Application Object
+    src = Source(SRC, SRC_TYPE)
+    app = DiscountInfo(args.app_config) if args.app else None
     
     # Define Color
     trg_color = get_random_color()
 
     # First frame
+    frame_idx = 0
     first_frame = src.get_first_frame()
     nets[ORG][HEIGHT], nets[ORG][WIDTH], nets[ORG][CHANNEL] = first_frame.shape
-    cv2.namedWindow(CV_WIN, cv2.WND_PROP_FULLSCREEN)
-    cv2.imshow(CV_WIN, first_frame)
     
-    frame_idx = 0
+    # Rescale Logo
+    logo = get_logo(
+        path        = LOGO, 
+        img_size    = (nets[ORG][HEIGHT], nets[ORG][WIDTH]), 
+        scale       = LOGO_SCALE 
+    )
+
+    # Define CV Windows
+    define_cv_window(
+        title       = CV_WIN,
+        first_frame = first_frame,
+        fullscreen  = True
+    )
+
     while True:
+
         # Get Input Data
         ret, image = src.get_frame()
         org_image = image.copy()
         t_start = time.time()
-        
+
         # Pre-process
         temp_image = preprocess(image, (nets[PRI][WIDTH], nets[PRI][HEIGHT]) )
 
         # Inference : result[<OUT_BLOB_NAME>] -> 1, 1, 200, 7
         nets[PRI][RESULT] = nets[PRI][EXEC].infer( inputs = { nets[PRI][IN_BLOB][0]: temp_image } )
 
-        # Run each bounding box
+        # Get output layer name
         out_blob = nets[PRI][OUT_BLOB][0]
+
+        # Run each bounding box
         for i in range(nets[PRI][RESULT][out_blob].shape[2]):
             
             # Parse bounding box
-            out = nets[PRI][RESULT][out_blob][0][0][i]
-            (image_idx, label_idx, conf), bbox = out[:3], out[3:] # -> [image_id, label, conf, x_min, y_min, x_max, y_max] 
+            (image_idx, label_idx, conf), bbox = parse_face_result( nets[PRI][RESULT][out_blob][0][0][i] )
 
             if conf > thres:
 
@@ -306,14 +87,8 @@ if __name__ == '__main__':
                 nets[SEC][RESULT] = nets[SEC][EXEC].infer( inputs = { nets[SEC][IN_BLOB][0]: temp_image } )
         
                 # Parse Result
-                dets_text, age, gender = "", None, None
-                for blob in nets[SEC][RESULT]:
-                    if blob == "age_conv3":
-                        age = int(nets[SEC][RESULT][blob][0][0][0][0]*100)
-                        
-                    else:
-                        prob = nets[SEC][RESULT][blob][0]
-                        gender = classes[str(np.argmax(prob))]
+                dets_text = ""
+                gender, age = parse_gender_age(nets[SEC][RESULT], classes)
                         
                 # Application
                 if args.app:
@@ -321,54 +96,65 @@ if __name__ == '__main__':
                     trg_color = app.get_info_color(discount_info)
                     dets_text += "{} ".format(discount_info)
                 else:
-                    dets_text = "{}, {}".format(age, gender)       
+                    dets_text = "Age {}, Gender {}".format(age, gender)       
 
                 # Draw Information Text
-                baseLine = cv2.getTextSize(dets_text, FONT_FACE, FONT_SCALE, FONT_THICK)[1]
-                text_x, text_y = x1, (y1 - baseLine)
+                dets_text_ls = [ " - " + cnt.strip() for cnt in dets_text.split(",") ]
 
                 # Draw Face Bounding Box
-                cv2.rectangle(org_image, (x1, y1), (x2, y2), trg_color, 2)
+                org_image = cv2.rectangle(org_image, (x1, y1), (x2, y2), trg_color, 2)
 
-                cv2.putText(org_image, dets_text, (text_x, text_y), FONT_FACE, FONT_SCALE, (0, 0, 0), FONT_THICK+1, cv2.LINE_AA)
-                cv2.putText(org_image, dets_text, (text_x, text_y), FONT_FACE, FONT_SCALE, trg_color, FONT_THICK, cv2.LINE_AA)
+                # Check if the Top out of limit
+                org_image = draw_gender_age(
+                    frame       = org_image, 
+                    xyxy        = (x1, y1, x2, y2),
+                    detections  = dets_text_ls,
+                    org_size    = ( nets[ORG][HEIGHT], nets[ORG][WIDTH] ),
+                    trg_color   = trg_color
+                )
 
         # Resize Whole Image
         org_image = cv2.resize(org_image, (nets[ORG][WIDTH], nets[ORG][HEIGHT]))
-        
-        # Draw information
-        org_image, end_pos = draw_device_info(org_image, ie, nets[SEC], None, "SECONDARY")
-        org_image, end_pos = draw_device_info(org_image, ie, nets[PRI], end_pos, "PRIMARY")
+        org_image = add_logo(org_image, logo)
 
+        # Get Temperature
+        temp = get_cpu_temp()
+        
         # Draw FPS
         t_end = time.time()
-        fps = round((1/(t_end - t_start)), 3)
-        cv2.putText(org_image, "FPS: {}".format(fps), (20, 20), FONT_FACE, FONT_SCALE, (0, 0, 0), FONT_THICK+1, cv2.LINE_AA)
-        cv2.putText(org_image, "FPS: {}".format(fps), (20, 20), FONT_FACE, FONT_SCALE, (0, 0, 255), FONT_THICK, cv2.LINE_AA)
+        fps = int((1/(t_end - t_start)))
+        informations = { 
+            "FPS"   : fps,
+            "TEMP"  : temp
+        } 
 
+        org_image = draw_information(
+            frame = org_image, 
+            informations = informations,
+            font_scale = 1.6
+        )
+        
         # Show result
         cv2.imshow(CV_WIN, org_image)  
         
         # Define Button Event
         key = cv2.waitKey(1)
+
         if key in [ ord('q'), 27 ]: 
-            # Break
-            logging.warning("Quit")
             break
-        if key == ord('c'):
+
+        elif key == ord('c'):
             if args.app:
-                logging.warning("Update Color")
                 app.generate_palette()
             else:
                 trg_color = get_random_color()
 
-        if key in [ ord('a'), 1, 123 ]: 
-            # Full Screen toggle with "a" , Space and F12
+        elif key in [ ord('a'), 1, 123 ]: 
             FULL_SCREEN = not FULL_SCREEN
             cv2.setWindowProperty(CV_WIN,cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN if FULL_SCREEN else cv2.WINDOW_NORMAL )
 
-        logging.warning(ie.get_metric(metric_name="DEVICE_THERMAL", device_name="MYRIAD"))
-
+        # Update Frame Index
         frame_idx += 1    
         
     cv2.destroyAllWindows()
+    logging.warning("Quit")
